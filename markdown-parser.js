9be07f7bcbf18aaa02702aace51111783780f965
nahid-linux-desktop <b.m.shamsnahid@gmail.com>
2020-12-29 23:10:51 +0600

    separate the functionalities

--- markdown-parser.js
+++ markdown-parser.js
@@ -1,8 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 const DirectoryStructureJSON = require('directory-structure-json');
 const fs = require('fs');
 
 const structureFormatter = (structure, parentPath = '', basePath) => {
-  return structure.map((node) => {
+  return structure.map(node => {
     const { type, name, children } = node;
     let formattedChildren = null;
     let formattedFileName = name;
@@ -27,7 +30,7 @@ const structureFormatter = (structure, parentPath = '', basePath) => {
       content:
         type === 'file'
           ? fs.readFileSync([basePath, path].join(''), 'utf8')
-          : '',
+          : ''
     };
   });
 };
@@ -43,11 +46,27 @@ const parseMarkdownTree = (basePath, cb) => {
   );
 };
 
-const runServer = () => {
-  
+const runServer = (
+  basePath = './public',
+  publicDirectoryPath = 'public',
+  routePath = '/tree',
+  port = 8081
+) => {
+  const app = express();
+
+  app.use(express.static(publicDirectoryPath));
+  app.use(cors());
+  app.use(morgan('combined'));
+
+  app.get(routePath, (req, res) => {
+    parseMarkdownTree(basePath, result => {
+      return res.json(result);
+    });
+  });
+
+  app.listen(port, () => console.log(`App is listening on port ${8081}`));
 };
 
 module.exports = {
-  parseMarkdownTree,
   runServer
 };
